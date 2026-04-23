e46c1e16cda297a5c1934efb8d6d7b1c136ebe70
trinhcongphat <trinhcongphat.06102003@gmail.com>
2026-04-23 14:59:13 +0700

    Time: 5 ms (33.40%) | Memory: 12.8 MB (43.38%) - LeetSync

---
+++ 1168-duplicate-zeros/duplicate-zeros.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def duplicateZeros(self, arr):
+        n = len(arr)
+        zeros = arr.count(0)
+        
+        i = n - 1
+        j = n + zeros - 1
+        
+        # Work backwards
+        while i >= 0:
+            if j < n:
+                arr[j] = arr[i]
+            
+            if arr[i] == 0:
+                j -= 1
+                if j < n:
+                    arr[j] = 0
+            
+            i -= 1
+            j -= 1
