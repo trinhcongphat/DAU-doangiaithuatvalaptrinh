811f1db580828c14ee64bbce491af4680d12f11a
trinhcongphat <trinhcongphat.06102003@gmail.com>
2026-04-23 14:56:28 +0700

    Time: 0 ms (100.00%) | Memory: 12.5 MB (20.80%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        jewel_set = set(jewels)
+        count = 0
+        
+        for stone in stones:
+            if stone in jewel_set:
+                count += 1
+                
+        return count
