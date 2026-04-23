25c099f89ffd438d4e05d034a5e56d7bba14c5f5
trinhcongphat <trinhcongphat.06102003@gmail.com>
2026-04-23 14:42:21 +0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (25.00%) - LeetSync

---
+++ 290-word-pattern/word-pattern.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        words = s.split()
+        
+        # Length must match
+        if len(pattern) != len(words):
+            return False
+        
+        char_to_word = {}
+        word_to_char = {}
+        
+        for c, w in zip(pattern, words):
+            
+            if c in char_to_word:
+                if char_to_word[c] != w:
+                    return False
+            else:
+                char_to_word[c] = w
+            
+            if w in word_to_char:
+                if word_to_char[w] != c:
+                    return False
+            else:
+                word_to_char[w] = c
+        
+        return True
