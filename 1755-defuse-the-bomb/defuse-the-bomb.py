b2c1f525ded61c9a1ad6e30025340a4bb6cb966f
trinhcongphat <trinhcongphat.06102003@gmail.com>
2026-05-12 08:01:15 +0700

    Time: 11 ms (34.60%) | Memory: 12.4 MB (25.95%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.py
@@ -0,0 +1,21 @@
+class Solution:
+    def decrypt(self, code, k):
+        n = len(code)
+        ans = [0] * n
+
+        if k == 0:
+            return ans
+
+        for i in range(n):
+            s = 0
+
+            if k > 0:
+                for j in range(1, k + 1):
+                    s += code[(i + j) % n]
+            else:
+                for j in range(1, -k + 1):
+                    s += code[(i - j + n) % n]
+
+            ans[i] = s
+
+        return ans
